dbd5bb3289b01eae8c0391fc7e4f3b2113e46e04
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-03-10 02:09:56 -0400

    created low budget ensemble

--- network1.py
+++ network1.py
@@ -10,16 +10,43 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 
+def linear_layer(input, num_inputs, num_outputs, relu=True):
+	weights = tf.Variable(tf.random.normal([num_inputs, num_outputs]))
+	biases = tf.Variable(tf.zeros([num_outputs]))
+	layer = tf.matmul(input, weights) + biases
+	if relu:
+		return tf.nn.relu(layer)
+	else:
+		return layer
+
+
 class NeuralNetwork:
+	def __init__(self, num_inputs, num_classes, learning_rate):
+		self.x = tf.placeholder(tf.float32, [None, num_inputs])
+		self.y = tf.placeholder(tf.float32, [None, num_classes])
+		
+		weights = tf.Variable(tf.zeros([num_inputs, num_classes]))
+		biases = tf.Variable([tf.zeros([num_classes])])
+		self.logits = tf.matmul(self.x, weights) + biases
+		
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.y)
+		self.loss = tf.reduce_mean(cross_entropy)
+		self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(self.loss)
+		
+		correct_prediction = tf.equal(tf.argmax(self.logits, axis=1), tf.argmax(self.y, axis=1))
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		self.prediction = tf.nn.softmax(self.logits)
+
+
+class Ensemble:
 	def __init__(self):
 		self.sess = tf.Session()
 		
+		self.models = 3
 		self.learning_rate = 0.5
 		self.batch_size = 128
 		
-		self.patience = 32
-		
-		self.num_input = 784
+		self.num_inputs = 784
 		self.num_classes = 10
 		
 		self.load_data()
@@ -34,70 +61,48 @@ class NeuralNetwork:
 		self.y_test = mnist.test.labels
 	
 	def build(self):
-		self.x = tf.placeholder(tf.float32, [None, self.num_input])
-		self.y = tf.placeholder(tf.float32, [None, self.num_classes])
+		self.networks = [NeuralNetwork(self.num_inputs, self.num_classes, self.learning_rate)
+							for _ in range(self.models)]
 		
-		self.weights = tf.Variable(tf.zeros([self.num_input, self.num_classes]))
-		self.biases = tf.Variable([tf.zeros([self.num_classes])])
-		self.logits = tf.matmul(self.x, self.weights) + self.biases
-		
-		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.y)
-		self.loss = tf.reduce_mean(cross_entropy)
-		self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
-		
-		correct_prediction = tf.equal(tf.argmax(self.logits, axis=1), tf.argmax(self.y, axis=1))
-		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-		self.prediction = tf.argmax(self.logits)
-	
 	def train(self, epochs=500):
 		self.sess.run(tf.global_variables_initializer())
-		total_train_loss = []
-		total_train_acc = []
-		total_valid_loss = []
-		total_valid_acc = []
-		best_acc = 0
-		no_acc_change = 0
 		
 		for e in range(epochs):
-			x_batch, y_batch = self.training_data.next_batch(self.batch_size)
-			feed_dict = {self.x: x_batch, self.y: y_batch}
-			self.sess.run(self.optimizer, feed_dict=feed_dict)
-			train_loss, train_acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
-			
-			feed_dict = {self.x: self.X_valid, self.y: self.y_valid}
-			valid_loss, valid_acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
-			
-			total_train_loss.append(train_loss)
-			total_train_acc.append(train_acc)
-			total_valid_loss.append(valid_loss)
-			total_valid_acc.append(valid_acc)
-			
-			print(f'epoch {e + 1}:',
-				f'train loss = {train_loss:.4f},',
-				f'train acc = {train_acc:.4f},',
-				f'valid loss = {valid_loss:.4f},',
-				f'valid acc = {valid_acc:.4f}'
-			)
-			
-			if valid_acc > best_acc:
-				best_acc = valid_acc
-				no_acc_change = 0
-			else:
-				no_acc_change += 1
-			
-			if no_acc_change >= self.patience:
-				print('early stopping')
-				break
+			for idx, net in enumerate(self.networks):
+				x_batch, y_batch = self.training_data.next_batch(self.batch_size)
+				feed_dict = {net.x: x_batch, net.y: y_batch}
+				self.sess.run(net.optimizer, feed_dict=feed_dict)
+				train_loss, train_acc = self.sess.run([net.loss, net.accuracy], feed_dict=feed_dict)
+				
+				feed_dict = {net.x: self.X_valid, net.y: self.y_valid}
+				valid_loss, valid_acc = self.sess.run([net.loss, net.accuracy], feed_dict=feed_dict)
+				
+				print(f'epoch {e + 1}, network {idx}:',
+					f'train loss = {train_loss:.4f},',
+					f'train acc = {train_acc:.4f},',
+					f'valid loss = {valid_loss:.4f},',
+					f'valid acc = {valid_acc:.4f}'
+				)
 		
 		print('training complete')
 		
-		feed_dict = {self.x: self.X_test, self.y: self.y_test}
-		acc = self.sess.run(self.accuracy, feed_dict=feed_dict)
-		print(f'test accuracy = {acc:.4f}')
-		
-		return total_train_loss, total_train_acc, total_valid_loss, total_valid_acc
-
+		for idx, net in enumerate(self.networks):
+			feed_dict = {net.x: self.X_test, net.y: self.y_test}
+			acc = self.sess.run(net.accuracy, feed_dict=feed_dict)
+			print(f'network {idx}: test accuracy = {acc:.4f}')
+	
+	def predict(self, x):
+		results = {}
+		for net in self.networks:
+			feed_dict = {net.x: x}
+			output = self.sess.run(tf.argmax(net.prediction, axis=1), feed_dict=feed_dict)
+			if output in results:
+				results[output] += 1
+			else:
+				results[output] = 1
+		return max(results, key=results.get)
+	
 
 if __name__ == '__main__':
-	net = NeuralNetwork()
-	net.train(500)
+	ensemble = Ensemble()
+	ensemble.train(500)
