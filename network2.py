5a9c33471664472dead9e102dff36d560f6b3a39
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-03-10 22:52:11 -0400

    finished ensemble model

--- network2.py
+++ network2.py
@@ -10,22 +10,31 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 
+def vote(tensor):
+	unique, _, counts = tf.unique_with_counts(tensor)
+	majority = tf.argmax(counts)
+	prediction = tf.gather(unique, majority)
+	return prediction
+
+
 class NeuralNetwork:
-	def __init__(self, num_inputs, num_classes, learning_rate):
-		self.x = tf.placeholder(tf.float32, [None, num_inputs])
-		self.y = tf.placeholder(tf.float32, [None, num_classes])
+	def __init__(self, num_classes, learning_rate):
+		x = tf.get_default_graph().get_tensor_by_name('ensemble/x:0')
+		y = tf.get_default_graph().get_tensor_by_name('ensemble/y:0')
 		
+		num_inputs = x.get_shape()[-1]
 		weights = tf.Variable(tf.zeros([num_inputs, num_classes]))
 		biases = tf.Variable([tf.zeros([num_classes])])
-		self.logits = tf.matmul(self.x, weights) + biases
+		self.logits = tf.matmul(x, weights) + biases
 		
-		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.y)
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=y)
 		self.loss = tf.reduce_mean(cross_entropy)
 		self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(self.loss)
 		
-		correct_prediction = tf.equal(tf.argmax(self.logits, axis=1), tf.argmax(self.y, axis=1))
+		self.prediction = tf.argmax(self.logits, axis=1)
+		correct_prediction = tf.equal(self.prediction, tf.argmax(y, axis=1))
 		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-		self.prediction = tf.nn.softmax(self.logits)
+		self.predict = tf.nn.softmax(self.logits)
 
 
 class Ensemble:
@@ -51,9 +60,17 @@ class Ensemble:
 		self.y_test = mnist.test.labels
 	
 	def build(self):
-		self.networks = [NeuralNetwork(self.num_inputs, self.num_classes, self.learning_rate)
-							for _ in range(self.models)]
-		self.loss = tf.group(*[net.loss for net in self.networks])
+		with tf.variable_scope('ensemble', reuse=tf.AUTO_REUSE) as scope:
+			self.x = tf.placeholder(tf.float32, [None, self.num_inputs], name='x')
+			self.y = tf.placeholder(tf.float32, [None, self.num_classes], name='y')
+		
+		self.networks = [NeuralNetwork(self.num_classes, self.learning_rate) for _ in range(self.models)]
+		self.loss = tf.reduce_mean([net.loss for net in self.networks])
+		
+		predictions = tf.stack([net.prediction for net in self.networks], axis=-1)
+		prediction = tf.map_fn(vote, predictions)
+		correct_prediction = tf.equal(prediction, tf.argmax(self.y, axis=1))
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 		
 	def train(self, epochs=500):
 		self.sess.run(tf.global_variables_initializer())
@@ -62,11 +79,11 @@ class Ensemble:
 			print(f'epoch {e + 1}')
 			for idx, net in enumerate(self.networks):
 				x_batch, y_batch = self.training_data.next_batch(self.batch_size)
-				feed_dict = {net.x: x_batch, net.y: y_batch}
+				feed_dict = {self.x: x_batch, self.y: y_batch}
 				self.sess.run(net.optimizer, feed_dict=feed_dict)
 				train_loss, train_acc = self.sess.run([net.loss, net.accuracy], feed_dict=feed_dict)
 				
-				feed_dict = {net.x: self.X_valid, net.y: self.y_valid}
+				feed_dict = {self.x: self.X_valid, self.y: self.y_valid}
 				valid_loss, valid_acc = self.sess.run([net.loss, net.accuracy], feed_dict=feed_dict)
 				
 				print(f'\tnetwork {idx}:',
@@ -78,25 +95,15 @@ class Ensemble:
 		
 		print('training complete')
 		
+		feed_dict = {self.x: self.X_test, self.y: self.y_test}
 		for idx, net in enumerate(self.networks):
-			feed_dict = {net.x: self.X_test, net.y: self.y_test}
 			loss, acc = self.sess.run([net.loss, net.accuracy], feed_dict=feed_dict)
 			print(f'network {idx}: test loss = {loss:.4f}, test acc = {acc:.4f}')
 		
-		
-	
-	def predict(self, x):
-		results = {}
-		for net in self.networks:
-			feed_dict = {net.x: x}
-			output = self.sess.run(tf.argmax(net.prediction, axis=1), feed_dict=feed_dict)
-			if output in results:
-				results[output] += 1
-			else:
-				results[output] = 1
-		return max(results, key=results.get)
+		loss, acc = self.sess.run([self.loss, self.accuracy], feed_dict=feed_dict)
+		print(f'test loss = {loss:.4f}, test acc = {acc:.4f}')
 	
 
 if __name__ == '__main__':
-	ensemble = Ensemble()
-	ensemble.train(500)
+	model = Ensemble()
+	model.train(300)
